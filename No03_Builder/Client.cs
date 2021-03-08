b9a2a706c499b35bb280b9d66e7ff710923f31f7
FelixSun <Felix.Sun@cn.ats.net>
2021-03-08 15:13:37 +0800

    perf(client):完善client与app.config代码

--- No03_Builder/Client.cs
+++ No03_Builder/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Reflection;
 using System.Text;
 
 namespace No03_Builder
@@ -14,11 +15,14 @@ namespace No03_Builder
             //配置文件，实现动态创建House
 
             //获取程序集
-            string assemblyName = Configuration.a[""];
+            string assemblyName =ConfigurationManager.AppSettings["AssemblyName"];
+            string builderName =ConfigurationManager.AppSettings["BuilderName"];
 
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type type = assembly.GetType("builderName");
+            Builder builder = Activator.CreateInstance(type);
             //固定某一种房子的创建
             House house = GameManager.CreateHouse(new RomanHouseBuilder());
-
         }
     }
 }
